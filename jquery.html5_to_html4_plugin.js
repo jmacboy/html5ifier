240b4c0921ed86cf1f2f6c5e18af21c78f54bf35
guillermo <guiye15@gmail.com>
2013-10-31 19:04:03 -0400

    Plugin Html5 mas el jquery con las validaciones de los navegadores

--- jquery.html5_to_html4_plugin.js
+++ jquery.html5_to_html4_plugin.js
@@ -1,71 +1,101 @@
 /* 
- * Html5ifier es un plugin jQuery que se utiliza para navegadores antiguos que no soportan html5 convirtiendo
- * el los elementos html5 a un div con class del nombre del elemento html5
+ * Html5ifier is a jQuery plugin that is used for older browsers that do not support html5 
+ * converting elements to a div with class name html5 element
  * 
- * Como Usar:
- * $('Elemento html5 a convertir').html5ifier();
- * esto convertira automaticamente todo el html5 en html4 
+ * how to use:
+ * $('Convert html5 element').html5ifier();
+ * this will convert automatically all in html4 html5 
  */
 
-$(document).ready(function(){
-    $.fn.html5ifier = function () {
-		//CREAMOS LA DTD 
-		var newDoctype = document.implementation.createDocumentType('html', '-//W3C//DTD XHTML 1.0 Transitional//EN', 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtdd');
-		//CAMBIAMOS LA DTD HTML5 A L A DTD XHMTL4.1
-		document.doctype.parentNode.replaceChild(newDoctype,document.doctype);
-        //find gets the tags from body that we are html5
-        var navElements = $('body').find("nav");
-        var footerElements = $('body').find("footer");
-        var headerElements = $('body').find("header");
-        //after we iterate over the elements we found we apply the function 'html5Element' to convert the html5 tag to a html4 div tag 
-        $.each(headerElements, function (i, e) {
-            html5Element(e);
-        });
-        $.each(navElements, function (i, e) {
-            html5Element(e);
-        });
-        $.each(footerElements, function (i, e) {
-            html5Element(e);
-        });
+jQuery(document).ready(function() {
 
-        function html5Element(e){
-            var elementoHTML5seleccionado=$(e); //contenido del elemento html5 seleccionado
-            for (var j = 0; j < elementoHTML5seleccionado.length; j++) {
-                var nombreElementoSeleccionado = elementoHTML5seleccionado[0].tagName;//sacamos el tipo de elemento html5 que es
-                //sacamos el elemento seleccionado en el caso de que existieran mas de uno igual
-                var elemento = elementoHTML5seleccionado[j]
-                //sacamos el contenido del elemento seleccionado
-                var ContenidoElementoSeleccionado = elemento.innerHTML;
-                //convertivos el nombre del elemento a minuscula
-                var nombreElementoSeleccionadoMinuscula = nombreElementoSeleccionado.toLowerCase();
-                //obtenemos los atributos del elemento y los colocamos en una variable como cadena de texto
-                var atributosElemento = "";
-                var elementoClase = null;
-                var atributos = elementoHTML5seleccionado[j].attributes;
-                for (var i = 0; i < atributos.length; i++) {
-                    var attr = atributos[i];
-                    //Verificamos si existe un atributo class para concatenarlo a nuestro atributo que se creara
-                    if (attr.name === "class") {
-                        elementoClase = attr.value + "'";
-                    } else {
-                        atributosElemento = atributosElemento + " " + attr.name + "='" + attr.value + "'";
+    $.fn.html5ifier = function() {
+        //Here we have selected element
+        var elementoHTML5seleccionado = $(this); //html5 element content selected
+        for (var j = 0; j < elementoHTML5seleccionado.length; j++) {
+            var nombreElementoSeleccionado = elementoHTML5seleccionado[0].tagName;//we get the type of element that is html5
+                         //we get the selected item in the event that there is more than one equal
+            var elemento = elementoHTML5seleccionado[j];
+            //we get the contents of the selected item
+            var ContenidoElementoSeleccionado = elemento.innerHTML;
+            //become the element name to lowercase
+            var nombreElementoSeleccionadoMinuscula = nombreElementoSeleccionado.toLowerCase();
+            //get the attributes of the element and put them in a variable as a text string
+            var atributosElemento = "";
+            var elementoClase = null;
+            var atributos = elementoHTML5seleccionado[j].attributes;
+            for (var i = 0; i < atributos.length; i++) {
+                var attr = atributos[i];
+                //Check to see if there is a class attribute to concatenate our attribute the creation
+                if (attr.value !== "null") {
+                    if (attr.value !== "0") {
+                        if (attr.value !== "") {
+                            if (attr.name === "class") {
+                                elementoClase = attr.value + "'";
+                            } else {
+                                atributosElemento = atributosElemento + " " + attr.name + "='" + attr.value + "'";
+                            }
+                        }
                     }
-                }
-                if (localStorage) {
-                    var ContenidoHTML4aInsertar;
-                    if (elementoClase !== null) {
-                        ContenidoHTML4aInsertar = "<div class=" + "'" + nombreElementoSeleccionadoMinuscula + " " + elementoClase + " " + atributosElemento + "> " + ContenidoElementoSeleccionado + " </div>";
-                    } else {
-                        ContenidoHTML4aInsertar = "<div class=" + "'" + nombreElementoSeleccionadoMinuscula + "' " + atributosElemento + "> " + ContenidoElementoSeleccionado + " </div>";
-                    }
-                    //insertamos el contenido de html4 como un hermano de el anterior elemento
-                    $(elemento).before(ContenidoHTML4aInsertar);
-                    //eliminamos el elemnto seleccionado 
-                    $(elemento).remove();
                 }
             }
-     
+
+            function getInternetExplorerVersion()
+                    // Returns the version of Internet Explorer or a -1 for other browsers.
+                    {
+                        var rv = -1;
+                        if (navigator.appName == 'Microsoft Internet Explorer')
+                        {
+                            var ua = navigator.userAgent;
+                            var re = new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");
+                            if (re.exec(ua) != null)
+                                rv = parseFloat(RegExp.$1);
+                        }
+                        return rv;
+                    }
+
+            var onload = false;
+            //get the internet explorer browser version
+            var version = getInternetExplorerVersion();
+            //if onload is true change to html4
+            if (version <= 7 && version > -1)
+            {
+                onload = true;
+            } else {
+                onload = false;
+
+            }
+            if (navigator.userAgent.toLowerCase().indexOf("Chrome") > -1) {
+                onload = false;
+            } else {
+                onload = true;
+            }
+            if (navigator.userAgent.toLowerCase().indexOf("Safari") >= -1 || navigator.userAgent.toLowerCase().indexOf("Firefox") >= -1 || navigator.userAgent.toLowerCase().indexOf("Opera") >= -1) {
+                onload = false;
+            } else {
+                onload = true;
+            }
+            if (onload) {
+               //CREATE THE DTD
+		var newDoctype = document.implementation.createDocumentType('html', '-//W3C//DTD XHTML 1.0 Transitional//EN', 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtdd');
+		//CHANGE TO THE HTML5 DTD XHMTL4.1
+		document.doctype.parentNode.replaceChild(newDoctype,document.doctype);
+                var ContenidoHTML4aInsertar;
+                if (elementoClase !== null) {
+                    ContenidoHTML4aInsertar = "<div class=" + "'" + nombreElementoSeleccionadoMinuscula + " " + elementoClase + " " + atributosElemento + "> " + ContenidoElementoSeleccionado + " </div>";
+                } else {
+                    ContenidoHTML4aInsertar = "<div class=" + "'" + nombreElementoSeleccionadoMinuscula + "' " + atributosElemento + "> " + ContenidoElementoSeleccionado + " </div>";
+                }
+                //html4 insert the contents of a brother of the former element
+                $(elemento).before(ContenidoHTML4aInsertar);
+                //remove the selected item
+                $(elemento).remove();
+            }
+
         }
-    }    
+    }
+
+
+
 });
 
